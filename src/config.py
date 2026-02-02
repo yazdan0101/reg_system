a7a167b90844199cf705748ad3886a7793df5d36
Yazdan Mohammadi <yazdan.mohammadi.dev@gmail.com>
2026-02-02 16:33:04 +0100

    add config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+# --- LLM Model Configuration ---
+LLM_MODEL: str = "llama-3.3-70b-versatile"
+LLM_MAX_NEW_TOKENS: int = 768
+LLM_TEMPERATURE: float = 0.01
+LLM_TOP_P: float = 0.95
+LLM_REPETITION_PENALTY: float = 1.03
+LLM_QUESTION: str = "What is the capital of France?"
